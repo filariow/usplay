e304034ce6b97096ed7b2119e6ada83ee88ff8f8
Francesco Ilario <fr.ilario@gmail.com>
2020-05-03 20:56:19 +0000

    Refactored Activity's Server List

--- internal/services/activity/api/server_list.go
+++ internal/services/activity/api/server_list.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 
+	"github.com/FrancescoIlario/usplay/internal/services/activity/storage"
 	"github.com/FrancescoIlario/usplay/pkg/services/activitycomm"
 	"github.com/FrancescoIlario/usplay/pkg/services/activitytypecomm"
 	"github.com/FrancescoIlario/usplay/pkg/services/ordercomm"
@@ -28,73 +29,24 @@ func (s *activityServer) List(ctx context.Context, req *activitycomm.ListActivit
 		return nil, status.Errorf(codes.Internal, "error retrieving the list of activities: %v", err)
 	}
 
-	// collecting the needed ActivityType details
-	var actTypes map[string]*activitytypecomm.ActivityType
-	var orders map[string]*ordercomm.Order
-	if len(acts) > 0 {
-		atrChan := make(chan map[string]*activitytypecomm.ActivityType)
-		go func() {
-			defer close(atrChan)
-
-			_ids := map[string]struct{}{}
-			for _, act := range acts {
-				_ids[act.ActivityTypeID] = struct{}{}
-			}
-			ids := []string{}
-			for k := range _ids {
-				if k != "" {
-					ids = append(ids, k)
-				}
-			}
-
-			activityTypesReply, err := s.actTypeCli.List(ctx,
-				&activitytypecomm.ListActivityTypesRequest{FilterIds: ids})
-
-			if err != nil {
-				log.Printf("error retrieving the filtered list of activity types: %v", err)
-			} else {
-				actTypes = make(map[string]*activitytypecomm.ActivityType, len(activityTypesReply.ActivityTypes))
-				for _, at := range activityTypesReply.ActivityTypes {
-					actTypes[at.Id] = at
-				}
-				atrChan <- actTypes
-			}
-		}()
-
-		ordChan := make(chan map[string]*ordercomm.Order)
-		go func() {
-			defer close(ordChan)
-
-			_ids := map[string]struct{}{}
-			for _, act := range acts {
-				_ids[act.OrderID] = struct{}{}
-			}
-			ids := []string{}
-			for k := range _ids {
-				if k != "" {
-					ids = append(ids, k)
-				}
-			}
-
-			ordersReply, err := s.orderCli.List(ctx,
-				&ordercomm.ListOrdersRequest{FilterIds: ids})
-
-			if err != nil {
-				log.Printf("error retrieving the filtered list of orders: %v", err)
-			} else {
-				orders = make(map[string]*ordercomm.Order, len(ordersReply.Orders))
-				for _, at := range ordersReply.Orders {
-					orders[at.Id] = at
-				}
-				ordChan <- orders
-			}
-		}()
-
-		<-ordChan
-		<-atrChan
+	if len(acts) == 0 {
+		return &activitycomm.ListActivitiesReply{Activities: nil}, nil
 	}
 
+	// collecting the needed ActivityType details
+	atrChan, ordChan := s.getActivities(ctx, acts), s.getOrders(ctx, acts)
+	orders, actTypes := <-ordChan, <-atrChan
+
 	// building response with optional data
+	activities := toActivityComm(acts, orders, actTypes)
+
+	return &activitycomm.ListActivitiesReply{
+		Activities: activities,
+	}, nil
+}
+
+func toActivityComm(acts storage.Activities, orders map[string]*ordercomm.Order,
+	actTypes map[string]*activitytypecomm.ActivityType) []*activitycomm.Activity {
 	activities := make([]*activitycomm.Activity, len(acts))
 	for idx, act := range acts {
 		// if info about the ActivityType was not retrieved, add the info you have
@@ -121,7 +73,71 @@ func (s *activityServer) List(ctx context.Context, req *activitycomm.ListActivit
 		}
 	}
 
-	return &activitycomm.ListActivitiesReply{
-		Activities: activities,
-	}, nil
+	return activities
+}
+
+func (s *activityServer) getOrders(ctx context.Context, acts storage.Activities) chan map[string]*ordercomm.Order {
+	ordChan := make(chan map[string]*ordercomm.Order)
+	go func() {
+		defer close(ordChan)
+
+		_ids := map[string]struct{}{}
+		for _, act := range acts {
+			_ids[act.OrderID] = struct{}{}
+		}
+		ids := []string{}
+		for k := range _ids {
+			if k != "" {
+				ids = append(ids, k)
+			}
+		}
+
+		ordersReply, err := s.orderCli.List(ctx,
+			&ordercomm.ListOrdersRequest{FilterIds: ids})
+
+		if err != nil {
+			log.Printf("error retrieving the filtered list of orders: %v", err)
+		} else {
+			orders := make(map[string]*ordercomm.Order, len(ordersReply.Orders))
+			for _, at := range ordersReply.Orders {
+				orders[at.Id] = at
+			}
+			ordChan <- orders
+		}
+	}()
+
+	return ordChan
+}
+
+func (s *activityServer) getActivities(ctx context.Context, acts storage.Activities) chan map[string]*activitytypecomm.ActivityType {
+	atrChan := make(chan map[string]*activitytypecomm.ActivityType)
+	go func() {
+		defer close(atrChan)
+
+		_ids := map[string]struct{}{}
+		for _, act := range acts {
+			_ids[act.ActivityTypeID] = struct{}{}
+		}
+		ids := []string{}
+		for k := range _ids {
+			if k != "" {
+				ids = append(ids, k)
+			}
+		}
+
+		activityTypesReply, err := s.actTypeCli.List(ctx,
+			&activitytypecomm.ListActivityTypesRequest{FilterIds: ids})
+
+		if err != nil {
+			log.Printf("error retrieving the filtered list of activity types: %v", err)
+		} else {
+			actTypes := make(map[string]*activitytypecomm.ActivityType, len(activityTypesReply.ActivityTypes))
+			for _, at := range activityTypesReply.ActivityTypes {
+				actTypes[at.Id] = at
+			}
+			atrChan <- actTypes
+		}
+	}()
+
+	return atrChan
 }
